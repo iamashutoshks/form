7abb45bee096e6a72c64390c4b36e7b5867c0170
Teresa Miyar <teresa.miyar@magnolia-cms.com>
2011-06-01 09:22:06 +0000

    MGNLFORM-88: summary 'template' display the fields title not the fields name
    git-svn-id: file:///home/dlipp/subversion-migration/svn-repo-copy/community/modules/magnolia-module-form/trunk@45756 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- src/main/java/info/magnolia/module/form/paragraphs/models/SummarySubStepFormModel.java
+++ src/main/java/info/magnolia/module/form/paragraphs/models/SummarySubStepFormModel.java
@@ -35,6 +35,7 @@ package info.magnolia.module.form.paragraphs.models;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -42,23 +43,24 @@ import java.util.Map;
 import javax.jcr.PathNotFoundException;
 import javax.jcr.RepositoryException;
 
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.Predicate;
-import org.apache.commons.collections.Transformer;
 import org.apache.commons.lang.StringUtils;
-
 
 import info.magnolia.cms.beans.config.ContentRepository;
 import info.magnolia.cms.core.Content;
 import info.magnolia.cms.core.ItemType;
+import info.magnolia.cms.core.Content.ContentFilter;
 import info.magnolia.cms.security.AccessDeniedException;
 import info.magnolia.cms.util.ContentUtil;
 import info.magnolia.cms.util.NodeDataUtil;
+import info.magnolia.cms.util.QueryUtil;
 import info.magnolia.context.MgnlContext;
 import info.magnolia.module.form.engine.FormState;
 import info.magnolia.module.form.engine.FormStepState;
 import info.magnolia.module.form.paragraphs.models.multistep.NavigationUtils;
 import info.magnolia.module.form.templates.FormParagraph;
+import info.magnolia.module.form.templates.ParagraphConfig;
+import info.magnolia.module.templating.Paragraph;
+import info.magnolia.module.templating.ParagraphManager;
 import info.magnolia.module.templating.RenderableDefinition;
 import info.magnolia.module.templating.RenderingModel;
 
@@ -89,22 +91,23 @@ public class SummarySubStepFormModel extends SubStepFormModel {
     }
 
     protected SummaryFormStepBean createSummaryFormStepBean(FormStepState step) {
-        Map<String, Object> parameters = step.getValues();
+        Map<String, Object> stepParameters = step.getValues();
         Map<String, Object> templateParams = new LinkedHashMap<String, Object>();
         try {
-            if(!parameters.isEmpty()) {
+            if(!stepParameters.isEmpty()) {
                 SummaryFormStepBean summaryFormStepBean = new SummaryFormStepBean();
                 String paragraphUUID = step.getParagraphUuid();
-                Content paragraph = ContentUtil.getContentByUUID(ContentRepository.WEBSITE, paragraphUUID);
-                Content page = getFormStepPage(paragraph);
+                Content contentParagraph = ContentUtil.getContentByUUID(ContentRepository.WEBSITE, paragraphUUID);
+                Content page = getFormStepPage(contentParagraph);
                 summaryFormStepBean.setName(page.getName());
                 summaryFormStepBean.setTitle(page.getTitle());
                 
-                List<String> fieldList = findStepFieldNames(paragraph);
+                Collection<Content> contentParagraphFieldList = findContentParagraphFields(contentParagraph);
                 
-                for(String controlName: fieldList) {
-                    if(parameters.containsKey(controlName)) {
-                        templateParams.put(controlName, parameters.get(controlName));
+                for(Content fieldNode: contentParagraphFieldList) {
+                    String controlName = NodeDataUtil.getString(fieldNode, "controlName");
+                    if(stepParameters.containsKey(controlName)) {
+                        findAndSetTemplateParameters(fieldNode, stepParameters, controlName, templateParams);
                     }
                 }
                 summaryFormStepBean.setParameters(templateParams);
@@ -116,7 +119,52 @@ public class SummarySubStepFormModel extends SubStepFormModel {
         return null;
     }
 
-    private Content getFormStepPage(Content paragraph) throws AccessDeniedException, PathNotFoundException, RepositoryException {
+    protected void findAndSetTemplateParameters(Content fieldNode, Map<String, Object> stepParameters, String controlName, Map<String, Object> templateParams) {
+        
+        String title = NodeDataUtil.getString(fieldNode, "title");
+        if(!NodeDataUtil.getString(fieldNode, "labels").isEmpty()) {
+            findAndSetComplexControlLabels(fieldNode, stepParameters, templateParams, controlName);
+        } else if(!title.isEmpty()){
+            templateParams.put(title, stepParameters.get(controlName));
+        }
+    }
+
+    /**
+     * Controls like checkbox, select dont have title, need to find the label of the option/s selected.
+     */
+    protected void findAndSetComplexControlLabels(Content fieldNode, Map<String, Object> stepParameters,
+            Map<String, Object> templateParams, String controlName) {
+        String stepParamvalue = (String) stepParameters.get(controlName);
+        if(StringUtils.isNotEmpty(stepParamvalue)) {
+            String[] stepfieldValues = stepParamvalue.split("_");
+            Map<String, String> controlValueLabelMap = fillControlValueLabelMap(fieldNode);
+            for (String value : stepfieldValues) {
+                if(!value.equals("")) {
+                    templateParams.put(controlValueLabelMap.get(value), "*");
+                }
+            }
+        }
+    }
+
+    private Map<String, String> fillControlValueLabelMap(Content fieldNode) {
+        Map<String, String> controlValueLabelMap = new HashMap<String,String>();
+        String controlLabels = NodeDataUtil.getString(fieldNode, "labels");
+        String[] labelsArray = controlLabels.split("\r\n");
+        for (String controlLabelValue : labelsArray) {
+            String[] labelValueArray = controlLabelValue.split(":");
+            if(labelValueArray.length > 0) {
+                String label = labelValueArray[0];
+                String value = label;
+                if(labelValueArray.length > 1) {
+                    value = labelValueArray[1];
+                }
+                controlValueLabelMap.put(value, label);
+            }
+        }//end for
+        return controlValueLabelMap;
+    }
+
+    protected Content getFormStepPage(Content paragraph) throws AccessDeniedException, PathNotFoundException, RepositoryException {
         
         Content page = paragraph;
         
@@ -141,33 +189,23 @@ public class SummarySubStepFormModel extends SubStepFormModel {
         return startParagraphNode;
     }
 
-    protected List<String> findStepFieldNames(Content paragraph) {
-        List<String> fieldList = new ArrayList<String>();
-        Collection<Content> fieldDefinitionList = ContentUtil.collectAllChildren(paragraph, ItemType.CONTENTNODE);
-        CollectionUtils.collect(fieldDefinitionList, new Transformer() {
-
-            public Object transform(Object input) {
-                Content node = (Content) input;
-                String template = node.getTemplate();
-                String controlName = NodeDataUtil.getString(node, "controlName");
-                if(StringUtils.isNotEmpty(controlName) && StringUtils.isNotEmpty(template) && !StringUtils.equals(template, "formHidden")) {
-                    return controlName;
-                }
-                return null;
-            }
+    protected Collection<Content> findContentParagraphFields(Content contentParagraph) {
+        
+        String query = "select * from " + ItemType.CONTENTNODE + " where jcr:path like '"
+                + contentParagraph.getHandle() +"/%' and controlName is not null";
+        return QueryUtil.query(ContentRepository.WEBSITE, query);
+    }
+    
+    protected List<ParagraphConfig> findFormDefinitionParagraphs() {
+        List<ParagraphConfig> paragraphsList = new ArrayList<ParagraphConfig>();
+        try {
+            Content formPage = getFormStartPage();
+            FormParagraph formDefinition = (FormParagraph)ParagraphManager.getInstance().getParagraphDefinition(formPage.getTemplate());
+            paragraphsList = formDefinition.getParagraphs();
             
-        }, fieldList);
-        //remove nulls
-        CollectionUtils.filter(fieldList, new Predicate() {
-
-            public boolean evaluate(Object object) {
-                if(object == null) {
-                    return false;
-                }
-                return true;
-            }
-            
-        });
-        return fieldList;
+        } catch (Exception e) {
+            throw new IllegalStateException("FormStepParagraph on page [" + content.getHandle() + "] could not find a FormParagraph in its parent");
+        }
+        return paragraphsList;
     }
 }
