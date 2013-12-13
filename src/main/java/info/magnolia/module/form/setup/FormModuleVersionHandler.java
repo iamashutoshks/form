63611aadd6e015e408d5f4748421a5d4c16bbc6f
ehechinger <eric.hechinger@magnolia-cms.com>
2013-12-13 12:58:04 +0100

    MGNLFORM-216 : Use the correct Inject API.

--- src/main/java/info/magnolia/module/form/setup/FormModuleVersionHandler.java
+++ src/main/java/info/magnolia/module/form/setup/FormModuleVersionHandler.java
@@ -55,7 +55,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import com.google.inject.Inject;
+import javax.inject.Inject;
 
 /**
  * VersionHandler for the form module.
