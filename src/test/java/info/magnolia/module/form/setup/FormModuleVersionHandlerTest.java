b57e614d22aaa42fb745657e349640f3a883058e
Evzen Fochr <evzen.fochr@magnolia-cms.com>
2014-08-25 13:47:46 +0200

    MGNLFORM-235 adding tests for update task and change to multi value validator

--- src/test/java/info/magnolia/module/form/setup/FormModuleVersionHandlerTest.java
+++ src/test/java/info/magnolia/module/form/setup/FormModuleVersionHandlerTest.java
@@ -193,6 +193,9 @@ public class FormModuleVersionHandlerTest extends ModuleVersionHandlerTestCase {
         // GIVEN
         this.setupConfigProperty("/modules/form/dialogs/formEdit/form/tabs/tabMain/fields/validation", "class", "info.magnolia.ui.form.field.definition.SelectFieldDefinition");
         this.setupConfigProperty("/modules/form/dialogs/formEdit/form/tabs/tabMain/fields/validation", "buttonLabel", "test");
+        Session websiteSession = MgnlContext.getJCRSession(RepositoryConstants.WEBSITE);
+        Node validationWebsiteNode = NodeUtil.createPath(websiteSession.getRootNode(), "fields/0", NodeTypes.ContentNode.NAME);
+        validationWebsiteNode.setProperty("validation","email");
 
         // WHEN
         executeUpdatesAsIfTheCurrentlyInstalledVersionWas(Version.parseVersion("2.2.5"));
@@ -203,6 +206,7 @@ public class FormModuleVersionHandlerTest extends ModuleVersionHandlerTestCase {
         assertThat(validationNode, hasProperty("leftColumnCaption", "dialog.form.edit.tabMain.validation.leftColumnCaption"));
         assertThat(validationNode, hasProperty("rightColumnCaption", "dialog.form.edit.tabMain.validation.rightColumnCaption"));
         assertFalse(validationNode.hasProperty("buttonLabel"));
+        assertTrue(validationWebsiteNode.getProperty("validation").isMultiple());
     }
 
     @Test
--- src/test/java/info/magnolia/module/form/templates/components/DefaultFormDataBinderTest.java
+++ src/test/java/info/magnolia/module/form/templates/components/DefaultFormDataBinderTest.java
@@ -34,28 +34,84 @@
 package info.magnolia.module.form.templates.components;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.*;
 
 import info.magnolia.context.MgnlContext;
+import info.magnolia.importexport.DataTransporter;
+import info.magnolia.module.form.FormModule;
 import info.magnolia.module.form.engine.FormStepState;
-import info.magnolia.test.MgnlTestCase;
+import info.magnolia.module.form.validators.Validator;
+import info.magnolia.rendering.template.TemplateAvailability;
+import info.magnolia.rendering.template.configured.ConfiguredTemplateAvailability;
+import info.magnolia.test.ComponentsTestUtil;
+import info.magnolia.test.RepositoryTestCase;
 import info.magnolia.test.mock.MockWebContext;
 import info.magnolia.test.mock.jcr.MockNode;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.jcr.ImportUUIDBehavior;
 import javax.jcr.Node;
 import javax.servlet.http.HttpServletRequest;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Tests for {@link DefaultFormDataBinder}.
  */
-public class DefaultFormDataBinderTest extends MgnlTestCase {
+public class DefaultFormDataBinderTest extends RepositoryTestCase {
+
+    DefaultFormDataBinder binder;
+    HttpServletRequest request;
+    MockWebContext ctx;
+    FormStepState step;
+
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        ComponentsTestUtil.setImplementation(TemplateAvailability.class, ConfiguredTemplateAvailability.class);
+
+        InputStream xmlStream = this.getClass().getClassLoader().getResourceAsStream("form.xml");
+        DataTransporter.importXmlStream(
+                xmlStream,
+                "website",
+                "/",
+                "name matters only when importing a file that needs XSL transformation",
+                false,
+                ImportUUIDBehavior.IMPORT_UUID_CREATE_NEW,
+                true,
+                true);
+        binder = new DefaultFormDataBinder();
+        request = mock(HttpServletRequest.class);
+        when(request.getParameterValues(anyString())).thenReturn(new String[] { "<", ">" });
+        ctx = (MockWebContext) MgnlContext.getWebContext();
+        ctx.setRequest(request);
+        FormModule formModule = new FormModule();
+        ArrayList validators= new ArrayList();
+        Validator validator1 = new Validator();
+        validator1.setName("test1");
+        Validator validator2 = new Validator();
+        validator2.setName("test2");
+        validators.add(validator1);
+        validators.add(validator2);
+        formModule.setValidators(validators);
+    }
+
+    @Override
+    @After
+    public void tearDown() throws Exception {
+        super.tearDown();
+        MgnlContext.setInstance(null);
+        ComponentsTestUtil.clear();
+    }
 
     @Test
     public void testFieldValueHaveToBeEscaped() throws Exception {
@@ -66,18 +122,31 @@ public class DefaultFormDataBinderTest extends MgnlTestCase {
         List<Node> list = new ArrayList<Node>();
         list.add(node);
         Iterator<Node> iterator = list.iterator();
-        FormStepState step = new FormStepState();
-
-        DefaultFormDataBinder binder = new DefaultFormDataBinder();
-        HttpServletRequest request = mock(HttpServletRequest.class);
-        when(request.getParameterValues(anyString())).thenReturn(new String[] { "<", ">" });
-        MockWebContext ctx = (MockWebContext) MgnlContext.getWebContext();
-        ctx.setRequest(request);
+        step = new FormStepState();
 
         //WHEN
         binder.bindAndValidateFields(iterator, step);
 
         //THEN
         assertEquals("&lt;__&gt;", step.get(controlName).getValue());
+    }
+
+    @Test
+    public void testFieldValueIsValidatedByMultivalueValidator() throws Exception {
+        //GIVEN
+        final String controlName = "controlName";
+        Node node = new MockNode();
+        node.setProperty("controlName", controlName);
+        node.setProperty("validation",new String[] {"test1","test2"});
+        List<Node> list = new ArrayList<Node>();
+        list.add(node);
+        Iterator<Node> iterator = list.iterator();
+        step = new FormStepState();
+
+        //WHEN
+        binder.bindAndValidateFields(iterator, step);
+
+        //THEN
+        assertTrue(step.isValid());
     }
 }
